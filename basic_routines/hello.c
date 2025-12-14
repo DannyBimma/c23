432fc3251585f140c16795a162961bbd371c5807
DannyBimma <dantrotman92@gmail.com>
2025-12-14 16:49:58 -0400

    Say hello to the world.

---
+++ basic_routines/hello.c
@@ -0,0 +1,13 @@
+/*
+ * Routine: A program that prints the greeting "Hello, world!" to
+ * the console.
+ *
+ * Author: DannyBimma
+ *
+ * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() { puts("Hello, world!"); }
