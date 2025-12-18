327134fc1c9fd00a12a22dfcceb186a8f46a193e
DannyBimma <dantrotman92@gmail.com>
2025-12-17 20:52:09 -0400

    Pointer logic.

--- basic_routines/swapv2.c
+++ basic_routines/swapv2.c
@@ -6,10 +6,12 @@
  * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
  */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 void swap(int *pa, int *pb);
+bool swapped(int *pa, int *pb);
 
 int main() {
   int a = 69;
@@ -18,6 +20,11 @@ int main() {
   swap(&a, &b);
 
   printf("main: a = %d, b = %d\n", a, b);
+  printf("main: a = %p, b = %p\n", &a, &b);
+
+  if (swapped(&a, &b)) {
+    printf("main: Swapped successfully!\n");
+  }
 
   return EXIT_SUCCESS;
 }
@@ -27,5 +34,16 @@ void swap(int *pa, int *pb) {
   *pa = *pb;
   *pb = temp;
 
+  // Print values
   printf("swap: a = %d, b = %d\n", *pa, *pb);
+  // Print pointer addresses
+  printf("swap: pa = %p, pb = %p\n", pa, pb);
+}
+
+// Check if values were swapped
+bool swapped(int *pa, int *pb) {
+  if (*pa == 420 && *pb == 69)
+    return true;
+  else
+    return false;
 }
