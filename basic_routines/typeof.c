ef2811d45caeaf8818085157ced907cdd18dfa8f
DannyBimma <dantrotman92@gmail.com>
2025-12-27 21:50:38 -0400

    Build some typeof program.

--- basic_routines/typeof.c
+++ basic_routines/typeof.c
@@ -6,6 +6,8 @@
  * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
  */
 
+#include <stdatomic.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 const _Atomic int a = 69;
@@ -19,14 +21,15 @@ const char *const strawhats[] = {
 };
 
 typeof_unqual(i) main() {
-  typeof_unqual(a) plain_i;           // plain_i is a plain int
-  typeof(_Atomic typeof(i)) atomic_i; // atomic_i is an _Atomic int
-  typeof(strawhats)
-      strawhats_array; // strawhats_array is an array of const char* const
+  typeof_unqual(a) plain_i;                      // plain_i is a plain int
+  typeof(_Atomic typeof_unqual(i)) atomic_i = 0; // atomic_i is an _Atomic int
+  typeof(strawhats) strawhats_array =
+      {}; // strawhats_array is an array of const char* const
   typeof_unqual(strawhats)
       strawhats_array2; // strawhats_array2 is an array of const char* const
 
-  printf("main: plain_i = %d, atomic_i = %d, strawhats_array = %p, "
+  printf("main:\n plain_i = %d\n atomic_i = %d\n strawhats_array = %p\n "
          "strawhats_array2 = %p\n",
-         plain_i, atomic_i, strawhats_array, strawhats_array2);
+         (int)plain_i, atomic_load(&atomic_i), (void *)strawhats_array,
+         (void *)strawhats_array2);
 }
