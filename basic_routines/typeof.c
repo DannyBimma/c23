ec34ea6f8ee97e7bddc92ddf218a2a8588a09662
DannyBimma <dantrotman92@gmail.com>
2025-12-27 21:09:17 -0400

    Init typeof example.

---
+++ basic_routines/typeof.c
@@ -0,0 +1,7 @@
+/*
+ * Routine: A program that shows the power if the typeof operator in C23.
+ *
+ * Author: DannyBimma
+ *
+ * Copyright (c) 2025 Technomancer Pirate Captain. All Rights Reserved.
+ */
